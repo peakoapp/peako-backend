21fe8efb4fa2590f3998dd4cc536e6f43699e12b
NataRich <natarichjay@gmail.com>
2022-10-08 11:58:05 -0500

    Configure jpa and mysql datasource in dev environment

---
+++ peako-core/src/main/java/com/peakoapp/core/config/SpringDataJpaConfiguration.java
@@ -0,0 +1,14 @@
+package com.peakoapp.core.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ *
+ */
+@Configuration
+@EnableJpaAuditing
+@EnableTransactionManagement
+public class SpringDataJpaConfiguration {
+}
