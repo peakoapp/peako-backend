f10a8c9d833536b6e7ba151f50fa4c5e576e604e
NataRich <natarichjay@gmail.com>
2022-10-08 20:41:37 -0500

    Add local identity provider

--- peako-core/src/main/java/com/peakoapp/core/constant/enums/IdentityProvider.java
+++ peako-core/src/main/java/com/peakoapp/core/constant/enums/IdentityProvider.java
@@ -1,4 +1,11 @@
 package com.peakoapp.core.constant.enums;
 
+/**
+ * The {@code IdentityProvider} enum class specifies the supported identity providers that the user
+ * can use to sign in their account.
+ *
+ * @version 0.1.0
+ */
 public enum IdentityProvider {
+    LOCAL,
 }
