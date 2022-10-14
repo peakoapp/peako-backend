d280d43ab6927bcc16e394bdb4a926eb97ed96ea
NataRich <natarichjay@gmail.com>
2022-10-14 09:39:54 -0500

    Add resource server

--- peako-resource-server/src/main/java/com/peakoapp/ResourceServerApplication.java
+++ peako-resource-server/src/main/java/com/peakoapp/ResourceServerApplication.java
@@ -4,13 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Starts the account microservice server.
+ * Starts the resource server.
  *
  * @version 0.1.0
  */
 @SpringBootApplication
-public class AccountServiceApplication {
+public class ResourceServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(AccountServiceApplication.class, args);
+        SpringApplication.run(ResourceServerApplication.class, args);
     }
 }
